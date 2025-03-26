0f5add13b0f9cd167c451ea988fc96fcf4380fea
liran0666 <ladelshten@gmail.com>
2025-03-26 13:19:27 +0200

    Add files via upload
    ט

---
+++ js.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// document.writeln("liran edelsthen")
+
+// const myname= {firstname : "liran", lastname: "edelshten"}
+// console.log(myname);
+
+// const threename= ["wow","shlomo","shmulik"]
+// console.log(threename);
+// alert("Hey ,hello");
+
+
+// let q2 = confirm('are you sure you want to add your name?');
+// console.log(q2);
+// if (q2==true){
+// let q1= prompt('whats your name?');
+// document.write(q1)}
+
+// const age= prompt("whats your age?");
+// console.log(age)
+
+// let q3= confirm('would you like an ice cream?');
+// if(age>40){
+//   alert("dont,its not good for you ,you old");
+// }
+// else{
+//   alert("thats fine go get some");
+// }
+let name1;
+let admin;
+name1='john';
+admin=name1;
+console.log(admin)
+const Amitired = true;
+console.log(Amitired);
+const Kakich= 'Vika Kich'
+document.write(Kakich)
+alert("שמע ישראל")
